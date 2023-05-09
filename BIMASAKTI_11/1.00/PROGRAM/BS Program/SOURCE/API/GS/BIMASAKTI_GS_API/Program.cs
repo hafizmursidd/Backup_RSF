754a40f3ceec35ed705966d239b657fb9e30119a
Hafiz Mursid <hafiz.codeid@realta.net>
2023-05-09 14:00:27 +0700

    first commit

---
+++ BIMASAKTI_11/1.00/PROGRAM/BS Program/SOURCE/API/GS/BIMASAKTI_GS_API/Program.cs
@@ -0,0 +1,16 @@
+using R_APIStartUp;
+using R_CrossPlatformSecurity;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.R_RegisterServices();
+
+builder.Services.AddSingleton<R_ISymmetricProvider, R_SymmetricAESProvider>();
+
+var app = builder.Build();
+
+app.R_SetupMiddleware();
+
+app.UseStaticFiles(); //blazor
+
+app.Run();
